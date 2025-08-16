b66377a8455f8461b271fd6a04c4989ce806136a
shiro <work.akk.shiro@mail.ru>
2025-08-17 00:43:34 +0300

    Добавил отрисовку границы подматрицы

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import MatrixRender from './components/MatrixRender'
 
+
 function App() {
 
-  const [matrix, setMatrix] = useState([[]])
-  const [result, setResult] = useState([])
+  const [matrix, setMatrix] = useState([[1, 3, 2], [2, 0, 0], [0, 0, 0]])
+  const [result, setResult] = useState([
+    {
+        leftBound: 1,
+        rightBound: 1,
+        topBound: 1,
+        bottomBound:1, 
+    }
+  ])
 
   const handleMatrixChange = (newMatrix) => {
     setMatrix(newMatrix)
--- src/components/MatrixRender.jsx
+++ src/components/MatrixRender.jsx
@@ -1,40 +1,102 @@
 import React, { useState, useEffect } from 'react'
-import './UI/ui'
+import './UI/ui.css'
 
 const MatrixRender = (props) => {
-    const handleCellChange = (row, col, value) => {
-        const newMatrix = [...matrix]
-        if (value.length > 1) {
+  const handleCellChange = (row, col, value) => {
+      const newMatrix = [...matrix]
+      if (value.length > 1) {
         value = value.slice(-1)
-        }
-        if (/^[0-9A-F]*$/.test(value)) {
+      }
+      if (/^[0-9A-F]*$/.test(value)) {
         newMatrix[row][col] = value || 0
-        setMatrix(newMatrix)
-        }
-    }
+        props.matrix
+      }
+  }
 
-    const isInSubmatrix = (row, col) => {
-    
-
-    }
-
-    return (
-      <div style={{ margin: '20px 0' }}>
-        <div className='matrix' style={{gridTemplateColumns: `repeat(${col}, 60px)`}}>
-          {props.matrix.map((row, rowIndex) => (
-            row.map((cell, colIndex) => (
-              <input 
-                className='cell'
-                key={`${rowIndex}-${colIndex}`}
-                type="text"
-                value={cell}
-                onChange={(e) => handleCellChange(rowIndex, colIndex, e.target.value)}
-              />
-            ))
-          ))}
-        </div>
-      </div>
+  const isInSubmatrix = (row, col) => {
+    return props.result.some(bounds =>
+      row >= bounds.topBound &&
+      row <= bounds.bottomBound &&
+      col >= bounds.leftBound &&
+      col <= bounds.rightBound
     )
+  }
+
+  const isTop = (row, col) => {
+    return props.result.some(bounds=>
+      row === bounds.topBound &&
+      col >= bounds.leftBound &&
+      col <= bounds.rightBound
+    )
+  }    
+
+  const isBottom = (row, col) =>{
+    return props.result.some(bounds=>
+      row === bounds.bottomBound &&
+      col >= bounds.leftBound &&
+      col <= bounds.rightBound
+    )
+  }
+    
+  const isLeft = (row, col) =>{
+    return props.result.some(bounds=>
+      col === bounds.leftBound &&
+      row >= bounds.topBound &&
+      row <= bounds.bottomBound
+    )
+  }
+
+  const isRight = (row, col) =>{
+    return props.result.some(bounds=>
+      col === bounds.rightBound &&
+      row >= bounds.topBound &&
+      row <= bounds.bottomBound
+    )
+  }
+
+  const highlightStyle = (bounds) => {
+    let numberCol = bounds.rightBound - bounds.leftBound + 1
+    let numberRow = bounds.bottomBound - bounds.topBound + 1
+    let numberSpaceBetwenCol = numberCol - 1
+    let numberSpaceBetwenRow = numberRow - 1
+    return (
+      {
+        top: (bounds.topBound ) * 66,
+        left: (bounds.leftBound ) * 66,
+        width: numberCol * 72,        
+        height: numberRow * 69,
+      }
+    )
+  }
+
+  return (
+    <div className="matrix-container" style={{ margin: '20px 0' }}>
+      <div className='matrix' style={{
+            gridTemplateColumns: `repeat(${props.matrix[0].length}, 60px)`, 
+            gridTemplateRows: `repeat(${props.matrix.lenght}, 60 px)`
+              }}>
+        {props.matrix.map((row, rowIndex) => (
+          row.map((cell, colIndex) => (
+            <input 
+              className={`cell ${isInSubmatrix(rowIndex, colIndex) ? "highlight" : ""}
+                ${isTop(rowIndex, colIndex) ? "border-top" : ""}
+                ${isBottom(rowIndex, colIndex) ? "border-bottom" : ""}
+                ${isLeft(rowIndex, colIndex) ? "border-left" : ""}
+                ${isRight(rowIndex, colIndex) ? "border-right" : ""}`}
+              key={`${rowIndex}-${colIndex}`}
+              type="text"
+              value={cell}
+              onChange={(e) => handleCellChange(rowIndex, colIndex, e.target.value)}
+            />
+          ))
+        ))}
+      </div>
+      {props.result.map((bounds, indexBound) => (
+        <div className="highlight2" style={highlightStyle(bounds)}></div>
+        ))
+      }
+    </div>
+  )
 }
 
 export default MatrixRender
